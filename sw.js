a4e1848b6b9e4f795a070519b0f687c6236457d0
nicoserwart <nicoserwart@gmail.com>
2026-04-06 20:11:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = 'trainingsplan-app-v1';
+const CACHE_NAME = 'trainingsplan-app-v2';
 const URLS_TO_CACHE = [
   './',
-  './trainingsplan_pwa.html',
+  './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
   self.clients.claim();
 });
